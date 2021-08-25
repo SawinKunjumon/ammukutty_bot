dfe9a870615b6616a796e7cc8faaef4ebc1acb08
SawinKunjumon <86658940+SawinKunjumon@users.noreply.github.com>
2021-08-25 18:31:24 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @cochinmovies",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰 JOIN OUR OTT UPDATES CHANNEL 🔰', url="https://t.me/cochinmoviesottup"
                                 )
                         ]
                     ]
